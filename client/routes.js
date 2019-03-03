f0b1a262aba1be69353c9caf3bc63a409f7398ae
Rebecca Chan <chan.rebecca@gmail.com>
2019-03-03 17:00:22 -0500

    remove login

--- client/routes.js
+++ client/routes.js
@@ -18,8 +18,6 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
         <Route path="/flightsurvey" component={FlightSurvey} />
         <Route path="/mainscreen" component={MainScreen} />
 
