41953c0e89a2d4c043652df7b8f62a46655026aa
Elcess <kelcess@alum.mit.edu>
2019-03-03 17:16:05 -0500

    add getOneCity

--- client/store/destination.js
+++ client/store/destination.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const GET_DESTINATION = 'GET_DESTINATION'
+const GET_ONE_CITY = 'GET_ONE_CITY'
 
 const destination = []
 
@@ -9,11 +10,25 @@ const getDestination = allDestinations => ({
   destination: allDestinations
 })
 
+const getOneCity = oneCity => ({
+  type: GET_ONE_CITY,
+  destination: [oneCity]
+})
+
 export const getCity = () => async dispatch => {
   try {
-    console.log('made it to thunk getCity')
     const res = await axios.get('/api/destinations')
     dispatch(getDestination(res.data || destination))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const getOneDestination = amenities => async dispatch => {
+  try {
+    console.log('made it to thunk getOneCity')
+    const res = await axios.get(`/api/destinations/:{amenities[0]}`)
+    dispatch(getOneCity(res.data || ''))
   } catch (err) {
     console.error(err)
   }
@@ -22,6 +37,8 @@ export const getCity = () => async dispatch => {
 export default function(state = destination, action) {
   switch (action.type) {
     case GET_DESTINATION:
+      return action.destination
+    case GET_ONE_CITY:
       return action.destination
     default:
       return state
