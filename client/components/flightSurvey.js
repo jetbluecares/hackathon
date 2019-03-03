61c58826e0a54bd2137a7f7aaebaa663e854ec08
Rebecca Chan <chan.rebecca@gmail.com>
2019-03-03 14:48:27 -0500

    survey questions

--- client/components/flightSurvey.js
+++ client/components/flightSurvey.js
@@ -2,28 +2,47 @@ import React from 'react'
 import {connect} from 'react-redux'
 import thumbsup from '../../public/thumbsup.png'
 import thumbsdown from '../../public/thumbsdown.png'
+import {Button} from '@material-ui/core'
 
-const FlightSurvey = () => {
-  return (
-    <div>
-      <h2>{this.question}</h2>
-      <div id="vote">
-        <img id="thumbsup" src={thumbsup} height="60" alt="thumbs up" />
-        <img id="thumbsdown" src={thumbsdown} height="60" alt="thumbs down" />
+const surveyQuestions = [
+  'How was your experience?',
+  'Would you recommend JetBlue to a friend?',
+  'The following questions are regarding the JetBlue travel process',
+  'How was the check in process?',
+  'How was navigating JetBlue.com?',
+  'Was the airport staff helpful?',
+  'Ease of passing through security?',
+  'Helpfulness of staff after security?',
+  'How was the airpot terminal?',
+  'How was the boarding process?'
+]
+
+class FlightSurvey extends Component {
+  constructor() {
+    super()
+    this.state = {
+      question: surveyQuestions[0]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.question}</h2>
+        <div id="vote">
+          <Button>
+            <img id="thumbsup" src={thumbsup} height="60" alt="thumbs up" />
+          </Button>
+          <Button>
+            <img
+              id="thumbsdown"
+              src={thumbsdown}
+              height="60"
+              alt="thumbs down"
+            />
+          </Button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-//Based on your experience, how would you rate your experience?
-
-//Rate your satisfaction with the following aspects of your travel planning experience with JetBlue:
-
-//How was the check in process?
-//How was navigating JetBlue.com?
-//How helpful was airport staff?
-//Ease of passing through security?
-//Helpfulness of staff after security?
-//Availaibility of flight information?
-//Airpot terminal?
-//Boarding process?
