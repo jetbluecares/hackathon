e170acd2c0e91b9a8913968af5afa5f5cd02b512
Emily Quinn <emkquinn@gmail.com>
2019-03-03 12:28:21 -0500

    add: image file

---
+++ client/images.js
@@ -0,0 +1,85 @@
+export const snacks = [
+  {
+    name: 'Pretzels',
+    imageUrl:
+      'https://www.fritolay.com/images/default-source/blue-bag-image/rold-gold-fat-free-tiny-twists.png?sfvrsn=1d52563a_4'
+  },
+  {
+    name: 'Pop Corners',
+    imageUrl:
+      'https://www.popcorners.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/o/popcorners_3oz_kettlecorn-2.png'
+  },
+  {
+    name: 'Terra Chips',
+    imageUrl:
+      'http://www.terrachips.com/wp-content/uploads/2017/06/Terra_Blues-5oz_122013.png'
+  },
+  {
+    name: 'Cookies',
+    imageUrl:
+      'https://belgianboys.com/wp-content/uploads/2018/06/222241_BelgianBoysChocOChip.jpg'
+  },
+  {
+    name: 'Cheez Itz',
+    imageUrl:
+      'https://www.dollargeneral.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/0/0/00800801_sunshine_cheez-it_cracker_3oz_main_1.jpg'
+  }
+]
+
+export const drinks = [
+  {
+    name: 'Coca Cola',
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/1669/0587/products/image_eb92a90c-b7fc-4137-8ef1-82061a79f3be_1024x1024.jpg?v=1546889600'
+  },
+  {
+    name: 'Diet Coke',
+    imageUrl: 'https://files.slack.com/files-pri/TGP2XH78E-FGMQHBKEW/image.png'
+  },
+  {
+    name: 'Ginger Ale',
+    imageUrl:
+      'https://www.staples-3p.com/s7/is/image/Staples/s0997980_sc7?wid=512&hei=512'
+  },
+  {
+    name: 'Seltzer Water',
+    imageUrl: ''
+  },
+  {
+    name: 'V8',
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/91-bnAeiUEL._SL1500_.jpg'
+  },
+  {
+    name: 'Orange Juice',
+    imageUrl:
+      'https://i5.walmartimages.com/asr/1027519a-aca4-44fb-83e7-3f1637159d25_1.4bc4714081ae1c3109dfd71b3e70df45.jpeg'
+  }
+]
+
+export const comfort = [
+  {
+    name: 'Blanket',
+    imageUrl: ''
+  },
+  {
+    name: 'Pillow',
+    imageUrl: ''
+  },
+  {
+    name: 'Ear Phones',
+    imageUrl: ''
+  },
+  {
+    name: 'Ear Plugs',
+    imageUrl: ''
+  },
+  {
+    name: 'Eye Mask',
+    imageUrl: ''
+  },
+  {
+    name: 'Facial Wipe',
+    imageUrl: ''
+  }
+]
