037b5a30bd30746f09b79031aff59c6d8af11ed0
Rebecca Chan <chan.rebecca@gmail.com>
2019-03-03 17:54:06 -0500

    CSS grid and jetblue logo update

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import JetBlue_Airways_Logo from '../../public/JetBlue_Airways_Logo.png'
+import jetbluelogo from '../../public/jetbluelogo.svg'
 import {Link} from 'react-router-dom'
 
 const Navbar = () => (
   <div>
     <nav>
       <Link to="/">
-        <img
-          id="logo"
-          src={JetBlue_Airways_Logo}
-          height="60"
-          alt="jetblue logo"
-        />
+        <img id="logo" src={jetbluelogo} height="60" alt="jetblue logo" />
       </Link>
     </nav>
   </div>
