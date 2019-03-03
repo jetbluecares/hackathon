325531264e1f6155176042fa45d455171576c221
Elcess <kelcess@alum.mit.edu>
2019-03-03 14:29:38 -0500

    add to destinations APIs

--- server/api/destinations.js
+++ server/api/destinations.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const {Destination} = require('../db/models')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -7,6 +9,22 @@ router.get('/', async (req, res, next) => {
     const destinations = await Destination.findAll({
       // explicitly select only the name and amenities fields
       attributes: ['name', 'amenities']
+    })
+    res.json(destinations)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:amenity', async (req, res, next) => {
+  try {
+    const amenity = req.params.amenity
+    const destinations = await Destination.findAll({
+      where: {
+        amenities: {
+          [Op.contains]: amenity
+        }
+      }
     })
     res.json(destinations)
   } catch (err) {
