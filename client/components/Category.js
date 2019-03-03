de2080decf196281c9aaaf73b478c9a89d478e9b
Emily Quinn <emkquinn@gmail.com>
2019-03-03 15:44:05 -0500

    feat: connected main screen and selection

--- client/components/Category.js
+++ client/components/Category.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+import {connect} from 'react-redux'
+import {setItems} from '../store/items'
+import {SelectionScreen} from '../components'
+
 const Category = props => {
+  const {items, name, handleClick} = props
+
+  console.log('made it category', props)
   return (
-    <div>
-      <h1>{props.title}</h1>
+    <div className="categories-container">
+      <button type="button" onClick={() => handleClick(items)}>
+        {name}{' '}
+      </button>
     </div>
   )
 }
+
+// const mapDispatch = dispatch => ({
+//   setItems: items => dispatch(setItems(items)),
+
+// })
 
 export default Category
--- client/routes.js
+++ client/routes.js
@@ -2,21 +2,22 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {MainScreen} from './components'
+import {MainScreen, SelectionScreen} from './components'
 import {me} from './store'
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData()
-  }
+  // componentDidMount() {
+  //   this.props.loadInitialData()
+  // }
 
   render() {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route exact path="/category" component={SelectionScreen} />
         <Route path="/" component={MainScreen} />
       </Switch>
     )
@@ -26,29 +27,14 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    loadInitialData() {
-      dispatch(me())
-    }
-  }
-}
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(Routes)
 
 /**
  * PROP TYPES
  */
-Routes.propTypes = {
-  loadInitialData: PropTypes.func.isRequired
-}
+// Routes.propTypes = {
+//   loadInitialData: PropTypes.func.isRequired
+// }
