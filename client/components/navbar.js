8246e14e815e98d9a65d8c180344ad2880440010
Rebecca Chan <chan.rebecca@gmail.com>
2019-03-03 12:35:08 -0500

    update navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,58 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import JetBlue_Airways_Logo from '../../public/JetBlue_Airways_Logo.png'
 import {MainScreen} from '../components'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>JetBlue</h1>
     <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/mainscreen">Main Screen</Link>
-        </div>
-      )}
+      <img
+        id="logo"
+        src={JetBlue_Airways_Logo}
+        height="40"
+        alt="piggybank logo"
+      />
     </nav>
+    <div>
+      {/* The navbar will show these links before you log in */}
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/mainscreen">Main Screen</Link>
+    </div>
     <hr />
   </div>
 )
-
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Navbar)
-
-/**
- * PROP TYPES
- */
-Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
